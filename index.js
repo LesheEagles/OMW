5432fa9136b42aaabe332d45e78db718c8471aab
LesheEagles <litches87@gmail.com>
2024-11-18 20:49:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"];
+  return days[date.getDay()]
+}
+  
 function getForecast(city){
  let apiKey = "6cef3c6b12ac03oa7c3c47900da5te44" 
   let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${city}key={apiKey}&units=metric`;
@@ -61,12 +67,14 @@ function getForecast(city){
 function displayForecast(response) 
 console.log(response.data);{
   let forecastHtml = "";
-  response.data.daily.forEach(function(day){
+  response.data.daily.forEach(function(day,index){
+    if (index < 5){
     forecastHtml =
       forecastHtml +
       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tue</div>
+        <div class="weather-forecast-date">${formatDay(day.time)</div>
+        
         <image src="${day.condition.icon_url}"class="weather-forecast-icon"/>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
